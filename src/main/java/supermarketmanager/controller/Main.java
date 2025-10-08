3ea6d2aeaaca71cac649f220398421a76cc6265c
chris <20111314@mail.wit.ie>
2025-10-08 22:39:07 +0100

    Quick testing of interation

--- src/main/java/supermarketmanager/controller/Main.java
+++ src/main/java/supermarketmanager/controller/Main.java
@@ -1,8 +1,24 @@
 package main.java.supermarketmanager.controller;
 
+import main.java.supermarketmanager.model.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        String something = "test";
-        System.out.println(something.toString());
+        LinkedList<String> test = new LinkedList<>();
+        test.add("A");
+        test.add("B");
+        test.add("C");
+        LinkedList<String> test2 = new LinkedList<>();
+        test2.add("D");
+        test2.add("E");
+        test2.add("F");
+        for(String s : test) {
+            System.out.println(s);
+            System.out.println("\n");
+        }
+        test.addAll(1 ,test2);
+        for(String s : test) {
+            System.out.println(s);
+        }
     }
 }
