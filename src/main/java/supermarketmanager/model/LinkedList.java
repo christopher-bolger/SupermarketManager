38dee70b266fbd202eb3f0fae2cc6aa3885151c9
Christopher <chris09332@gmail.com>
2025-10-08 15:01:32 +0100

    Decided to implement list so im basically back to square one (or at least it feels like it)

--- src/main/java/supermarketmanager/model/LinkedList.java
+++ src/main/java/supermarketmanager/model/LinkedList.java
@@ -1,14 +1,27 @@
 package main.java.supermarketmanager.model;
 
-import java.util.Iterator;
-import java.util.Objects;
-import java.util.Spliterator;
+import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
 
-//TODO
-//Look at implementing Iterable & Iterator -> On the notes
-//Maybe also implement List
-public class LinkedList<E>{
+//Questions:
+// Contracts?
+// Adding node behaviour? Should I always have one extra or can I try keep the list to its actual size?
+// Explanation for iterator methods
+// Iterator implementations needed for removeFirst & removeLast?
+// spliterator???
+// stream??
+// list.super()?
+// addLast just add?
+// Handle collection passed to method?
+// replaceAll unaryOperator?
+// toArray
+// Why do some pass objects and others allow for E?
+
+public class LinkedList<E> implements List<E>{
     private Node<E> head, tail;
 
     public LinkedList() {
@@ -16,23 +29,24 @@ public class LinkedList<E>{
         tail = head;
     }
 
+    // TODO Contract?
     // Add element/node methods
     public boolean add(E element) {
         if(element == null)
             return false;
-
-        if(tail.getContent() != null)
-            addNode();
-        return tail.setContent(element);
+        tail.setContent(element);
+        addNode();
+        return true;
     }
 
-    public boolean insert(int index, E element) {
+    @Override
+    public void add(int index, E element) {
         if(!checkIndex(index) || element == null)
-            return false;
+            return;
 
         if(index == 0){
             head = new Node<>(element, head);
-            return true;
+            return;
         }
 
         Node<E> node = head;
@@ -44,7 +58,6 @@ public class LinkedList<E>{
 
         Node<E> nextLink = node.next;
         node.next = new Node<>(element, nextLink);
-        return true;
     }
 
     private void addNode(){
@@ -56,6 +69,13 @@ public class LinkedList<E>{
     //TODO
     //Needs work
     public E get(E element){
+       Node<E> temp=head;
+        //while(temp!=null && !temp.getContent().equals(element)) temp=temp.next;
+        for(temp=head; temp!=null && !temp.getContent().equals(element); temp=temp.next);
+        return temp==null ? null : temp.getContent();
+
+
+
         if(isEmpty())
             return null;
         if(size() == 1)
@@ -69,7 +89,7 @@ public class LinkedList<E>{
                 break;
             }
             node = node.next;
-        }while(node != null);
+        }while(node.getContent() != null);
 
         return found ? node.getContent() : null;
     }
@@ -88,47 +108,183 @@ public class LinkedList<E>{
         return null;
     }
 
-    public E getFirst(){
-        return head.getContent();
-    }
-
-    public E getLast(){
-        return tail.getContent();
-    }
-
-    //removal methods
-    public boolean remove(int index){
+    @Override
+    public E set(int index, E element) {
         if(!checkIndex(index))
-            return false;
-
-        if(index == 0) {
-            head = head.next;
-            return true;
-        }
+            return null;
 
         Node<E> temp = head;
         int count = 0;
-        while(count != index-1){
+        while(count != index){
             temp = temp.next;
             count++;
         }
-        if(temp.next == tail) {
-            tail = temp;
-            tail.next = null;
-        }else
-            temp.next = temp.next.next;
-        return true;
+        temp.setContent(element);
+        return temp.getContent();
     }
 
-//    TODO
+    public E getFirst(){
+        return size() > 0 ? head.getContent() : null;
+    }
+
+    public E getLast(){
+        return size() > 0 ? tail.getContent() : null;
+    }
+
+    @Override
+    public E removeFirst() {
+        Node<E> temp = head;
+        head = head.next;
+        return temp.getContent();
+    }
+
+    @Override
+    public E removeLast() {
+        Node<E> temp = head;
+
+        int indexToRemove = size()-2;
+        int index = 0;
+
+        while(index != indexToRemove){
+            temp = temp.next;
+            index++;
+        }
+
+        Node<E> removedObject = temp.next;
+        temp.next = new Node<>();
+        tail = temp.next;
+        return removedObject.getContent();
+    }
+
+    @Override
+    public List<E> reversed() {
+        return List.super.reversed();
+    }
+
+    //removal methods
+    public E remove(int index){
+        if(!checkIndex(index))
+            return null;
+
+        Node<E> removed = head;
+        if(index == 0) {
+            head = head.next;
+            return head.getContent();
+        }
+
+        int count = 0;
+        while(count != index-1){
+            removed = removed.next;
+            count++;
+        }
+        if(removed.next == tail) {
+            tail = removed;
+            addNode();
+        }else
+            removed.next = removed.next.next;
+        return removed.getContent();
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        if(isEmpty())
+            return -1;
+        int index = 0;
+        Node<E> temp = head;
+        while(temp.getContent() != null){
+            if(temp.getContent().equals(o))
+                break;
+            temp = temp.next;
+            index++;
+        }
+        return index;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        if(isEmpty())
+            return -1;
+
+        int index = 0;
+        int foundIndex = -1;
+        Node<E> temp = head;
+
+        while(temp.getContent() != null){
+            if(temp.getContent().equals(o))
+                foundIndex = index;
+            temp = temp.next;
+            index++;
+        }
+        return foundIndex;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        if(isEmpty() || !checkIndex(fromIndex) || !checkIndex(toIndex))
+            return List.of();
+
+        int index = 0;
+        Node<E> temp = head;
+        while(index != fromIndex){
+            temp = temp.next;
+            index++;
+        }
+
+        List<E> returnList = new LinkedList<E>();
+        while(index != toIndex+1){
+            E object = temp.getContent();
+            returnList.add(object);
+            temp = temp.next;
+            index++;
+        }
+        return returnList;
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        return List.super.spliterator();
+    }
+
+    @Override
+    public Stream<E> stream() {
+        return List.super.stream();
+    }
+
+    @Override
+    public Stream<E> parallelStream() {
+        return List.super.parallelStream();
+    }
+
+    @Override
+    public void addFirst(E e) {
+        List.super.addFirst(e);
+    }
+
+    @Override
+    public void addLast(E e) {
+        List.super.addLast(e);
+    }
+
+    //    TODO
 // I feel like this is overly complicated
-    public boolean remove(E element){
+    public boolean remove(Object element){
         if(element == null || isEmpty())
             return false;
 
         if(size() == 1)
             if(head.getContent().equals(element)) {
-                return clear();
+                clear();
+                return true;
             }else return false;
 
         if(head.getContent().equals(element)){
@@ -154,24 +310,60 @@ public class LinkedList<E>{
         }
     }
 
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super E> filter) {
+        return List.super.removeIf(filter);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<E> operator) {
+        List.super.replaceAll(operator);
+    }
+
+    @Override
+    public void sort(Comparator<? super E> c) {
+        List.super.sort(c);
+    }
+
     //misc methods
     private boolean checkIndex(int index){
         return index > -1 && index < size();
     }
 
-    public boolean clear(){
+    public void clear(){
         head = new Node<>();
         tail = head;
-        return true;
     }
 
     public int size(){
-        if(head == null || head.getContent() == null)
-            return 0;
-
         Node<E> node = head;
-        int count = 1;
-        while(node.next != null){
+        int count = 0;
+        while(node.getContent() != null){
             count++;
             node = node.next;
         }
@@ -180,6 +372,36 @@ public class LinkedList<E>{
 
     public boolean isEmpty(){
         return head.getContent() == null;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new NodeIterator<>(head, this);
+    }
+
+    @Override
+    public void forEach(Consumer<? super E> action) {
+        List.super.forEach(action);
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public <T> T[] toArray(IntFunction<T[]> generator) {
+        return List.super.toArray(generator);
     }
 
     public String toString(){
@@ -193,46 +415,5 @@ public class LinkedList<E>{
             node = node.next;
         }
         return string.toString();
-    }
-
-    private class Node<E>{
-        private E content;
-        public Node<E> next;
-
-        public Node(){
-            content = null;
-            next = null;
-        }
-
-        public Node(E content, Node<E> next){
-            if(next != null) {
-                setContent(content);
-                this.next = next;
-            }
-        }
-
-        public boolean setContent(E content){
-            if(content != null) {
-                this.content = content;
-                return true;
-            }
-            return false;
-        }
-
-        public E getContent(){
-            return content;
-        }
-
-        public String toString(){
-            return content.toString();
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Node<?> node = (Node<?>) o;
-            return Objects.equals(content, node.content) && Objects.equals(next, node.next);
-        }
     }
 }
--- test/main/java/supermarketmanager/model/LinkedListTest.java
+++ test/main/java/supermarketmanager/model/LinkedListTest.java
@@ -3,6 +3,9 @@ package main.java.supermarketmanager.model;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
@@ -39,148 +42,178 @@ class LinkedListTest {
     }
 
     @Test
-    void testAdd(){
-        assertTrue(emptyList.add("HI"));
-        assertTrue(emptyList.add("HIIII"));
-        assertTrue(emptyList.add("Howdy"));
-        assertFalse(emptyList.add(null));
-        assertEquals(3, emptyList.size());
+    void testSet(){
+        assertNull(emptyList.set(0, "HII"));
+        assertEquals("Evening", listWithElements.set(3, "Evening"));
+        assertNull(listWithElements.get("Morning")); // the object that was replaced
     }
 
     @Test
-    void testInsert(){
-        assertTrue(listWithElements.insert(2, "position 2"));
-        assertTrue(listWithElements.insert(4, "position 4"));
-        assertFalse(listWithElements.insert(0, null));
-        assertFalse(listWithElements.insert(6, "not possible")); // 6 elements in array but it counts from 0
-        assertFalse(listWithElements.insert(-1, "not possible"));
-        assertFalse(emptyList.insert(0, "EmptyList, can't insert"));
-        emptyList.add("FirstElement");
-        assertTrue(emptyList.insert(0, "Now it should work"));
+    void testIndexOf(){
+        assertEquals(-1, emptyList.indexOf("Something"));
+        assertEquals(0, listWithOneElement.indexOf("Single"));
+        assertEquals(3, listWithElements.indexOf("Morning"));
     }
 
     @Test
-    void testGet(){
-        assertEquals("Morning", listWithElements.get(listWithElements.size()-1));
-        assertEquals("Hi", listWithElements.get("Hi"));
-        assertEquals("Hi", listWithElements.get(0));
-        assertEquals("Single", listWithOneElement.getFirst());
-        assertEquals("Single", listWithOneElement.get(0));
-        assertEquals("Single", listWithOneElement.get("Single"));
-        assertNull(listWithOneElement.get(null));
-        assertNull(listWithElements.get("NotInTheList"));
-        assertNull(listWithElements.get(listWithElements.size()));
-        assertTrue(listWithElements.add("Good Morning"));
-        assertEquals("Good Morning", listWithElements.getLast());
+    void testLastIndexOf(){
+        assertEquals(-1, emptyList.lastIndexOf("Something"));
+        assertEquals(0, listWithOneElement.lastIndexOf("Single"));
+        assertEquals(3, listWithElements.lastIndexOf("Morning"));
+        listWithElements.add("Morning");
+        assertEquals(4, listWithElements.lastIndexOf("Morning"));
     }
 
     @Test
-    void testGetFirst(){
-        assertNull(emptyList.getFirst());
-        assertEquals("Hi", listWithElements.getFirst());
-        listWithElements.insert(0, "iH");
-        assertEquals("iH", listWithElements.getFirst());
-        listWithElements.insert(0, "hmm");
-        //Making sure there's no data loss
-        assertEquals("hmm", listWithElements.getFirst());
-        assertEquals("iH", listWithElements.get(1));
+    void testSubList(){
+        List<String> testList = emptyList.subList(0,0);
+        assertEquals(0, testList.size());
     }
 
-    @Test
-    void testGetLast(){
-        assertNull(emptyList.getLast());
-        assertEquals("Morning", listWithElements.getLast());
-        listWithElements.add("Evening");
-        assertEquals("Evening", listWithElements.getLast());
-    }
-
-    @Nested
-    class testRemoveByIndex {
-        @Test
-        void testEmptyList(){
-            assertFalse(emptyList.remove(0));
-        }
-
-        @Test
-        void testSingleItemList(){
-            assertTrue(listWithOneElement.remove(0));
-            assertFalse(listWithOneElement.remove(1));
-            assertFalse(listWithOneElement.remove(0));
-        }
-
-        @Test
-        void testMultiItemList(){
-            assertTrue(listWithElements.remove(1));
-            assertNull(listWithElements.get(3)); // is now size() 3 so index 3 doesn't exist
-            String check = listWithElements.toString();
-            assertFalse(check.contains("Howdy"));
-            assertTrue(listWithElements.remove(listWithElements.size()-1));
-            assertEquals(2, listWithElements.size());
-            check = listWithElements.toString();
-            assertFalse(check.contains("Morning"));
-            assertNull(listWithElements.get(3)); // "Morning" was removed, size is now 2;
-            assertTrue(listWithElements.remove(listWithElements.size()-1));
-            assertEquals(1, listWithElements.size());
-        }
-    }
-
-    @Nested
-    class testRemoveByElement{
-        @Test
-        void testEmptyList(){
-            assertFalse(emptyList.remove(null));
-            assertFalse(emptyList.remove("Nothing"));
-        }
-
-        @Test
-        void testSingleItemList(){
-            assertFalse(listWithOneElement.remove(null));
-            assertFalse(listWithOneElement.remove("Not in List"));
-            assertTrue(listWithOneElement.remove("Single"));
-            assertNull(listWithOneElement.get("Single"));
-        }
-
-        @Test
-        void testMultiItemList(){
-            assertFalse(listWithElements.remove(null));
-            assertEquals(4, listWithElements.size());
-
-            assertTrue(listWithElements.remove("Hi"));
-            assertFalse(listWithElements.toString().contains("Hi"));
-            assertEquals(3, listWithElements.size());
-            assertEquals("Howdy", listWithElements.getFirst());
-
-            assertFalse(listWithElements.remove("Not in List"));
-            assertNotNull(listWithElements.get("Howdy"));
-            assertTrue(listWithElements.remove("Howdy"));
-            assertNull(listWithElements.get("Howdy"));
-            assertEquals("Hello", listWithElements.getFirst());
-        }
-    }
-
-    @Test
-    void testClear(){
-        assertTrue(listWithOneElement.clear());
-        assertTrue(emptyList.clear());
-        assertTrue(listWithOneElement.clear());
-    }
-
-    @Test
-    void testToString(){
-        String string = emptyList.toString();
-        assertNull(string);
-        string = listWithOneElement.toString();
-        assertTrue(string.contains("Single"));
-        string = listWithElements.toString();
-        assertTrue(string.contains("Hi"));
-        assertTrue(string.contains("Howdy"));
-        assertTrue(string.contains("Hello"));
-        assertTrue(string.contains("Morning"));
-        listWithOneElement.remove("Single");
-        string = listWithOneElement.toString();
-        assertNull(string);
-        listWithOneElement.add("Single");
-        string = listWithOneElement.toString();
-        assertNotNull(string);
-    }
+//
+//    @Test
+//    void testAdd(){
+//        assertTrue(emptyList.add("HI"));
+//        assertTrue(emptyList.add("HIIII"));
+//        assertTrue(emptyList.add("Howdy"));
+//        assertFalse(emptyList.add(null));
+//        assertEquals(3, emptyList.size());
+//    }
+//
+//    @Test
+//    void testInsert(){
+//        assertTrue(listWithElements.insert(2, "position 2"));
+//        assertTrue(listWithElements.insert(4, "position 4"));
+//        assertFalse(listWithElements.insert(0, null));
+//        assertFalse(listWithElements.insert(6, "not possible")); // 6 elements in array but it counts from 0
+//        assertFalse(listWithElements.insert(-1, "not possible"));
+//        assertFalse(emptyList.insert(0, "EmptyList, can't insert"));
+//        emptyList.add("FirstElement");
+//        assertTrue(emptyList.insert(0, "Now it should work"));
+//    }
+//
+//    @Test
+//    void testGet(){
+//        assertEquals("Morning", listWithElements.get(listWithElements.size()-1));
+//        assertEquals("Hi", listWithElements.get("Hi"));
+//        assertEquals("Hi", listWithElements.get(0));
+//        assertEquals("Single", listWithOneElement.getFirst());
+//        assertEquals("Single", listWithOneElement.get(0));
+//        assertEquals("Single", listWithOneElement.get("Single"));
+//        assertNull(listWithOneElement.get(null));
+//        assertNull(listWithElements.get("NotInTheList"));
+//        assertNull(listWithElements.get(listWithElements.size()));
+//        assertTrue(listWithElements.add("Good Morning"));
+//        assertEquals("Good Morning", listWithElements.getLast());
+//    }
+//
+//    @Test
+//    void testGetFirst(){
+//        assertNull(emptyList.getFirst());
+//        assertEquals("Hi", listWithElements.getFirst());
+//        listWithElements.insert(0, "iH");
+//        assertEquals("iH", listWithElements.getFirst());
+//        listWithElements.insert(0, "hmm");
+//        //Making sure there's no data loss
+//        assertEquals("hmm", listWithElements.getFirst());
+//        assertEquals("iH", listWithElements.get(1));
+//    }
+//
+//    @Test
+//    void testGetLast(){
+//        assertNull(emptyList.getLast());
+//        assertEquals("Morning", listWithElements.getLast());
+//        listWithElements.add("Evening");
+//        assertEquals("Evening", listWithElements.getLast());
+//    }
+//
+//    @Nested
+//    class testRemoveByIndex {
+//        @Test
+//        void testEmptyList(){
+//            assertFalse(emptyList.remove(0));
+//        }
+//
+//        @Test
+//        void testSingleItemList(){
+//            assertTrue(listWithOneElement.remove(0));
+//            assertFalse(listWithOneElement.remove(1));
+//            assertFalse(listWithOneElement.remove(0));
+//        }
+//
+//        @Test
+//        void testMultiItemList(){
+//            assertTrue(listWithElements.remove(1));
+//            assertNull(listWithElements.get(3)); // is now size() 3 so index 3 doesn't exist
+//            String check = listWithElements.toString();
+//            assertFalse(check.contains("Howdy"));
+//            assertTrue(listWithElements.remove(listWithElements.size()-1));
+//            assertEquals(2, listWithElements.size());
+//            check = listWithElements.toString();
+//            assertFalse(check.contains("Morning"));
+//            assertNull(listWithElements.get(3)); // "Morning" was removed, size is now 2;
+//            assertTrue(listWithElements.remove(listWithElements.size()-1));
+//            assertEquals(1, listWithElements.size());
+//        }
+//    }
+//
+//    @Nested
+//    class testRemoveByElement{
+//        @Test
+//        void testEmptyList(){
+//            assertFalse(emptyList.remove(null));
+//            assertFalse(emptyList.remove("Nothing"));
+//        }
+//
+//        @Test
+//        void testSingleItemList(){
+//            assertFalse(listWithOneElement.remove(null));
+//            assertFalse(listWithOneElement.remove("Not in List"));
+//            assertTrue(listWithOneElement.remove("Single"));
+//            assertNull(listWithOneElement.get("Single"));
+//        }
+//
+//        @Test
+//        void testMultiItemList(){
+//            assertFalse(listWithElements.remove(null));
+//            assertEquals(4, listWithElements.size());
+//
+//            assertTrue(listWithElements.remove("Hi"));
+//            assertFalse(listWithElements.toString().contains("Hi"));
+//            assertEquals(3, listWithElements.size());
+//            assertEquals("Howdy", listWithElements.getFirst());
+//
+//            assertFalse(listWithElements.remove("Not in List"));
+//            assertNotNull(listWithElements.get("Howdy"));
+//            assertTrue(listWithElements.remove("Howdy"));
+//            assertNull(listWithElements.get("Howdy"));
+//            assertEquals("Hello", listWithElements.getFirst());
+//        }
+//    }
+//
+//    @Test
+//    void testClear(){
+//        assertTrue(listWithOneElement.clear());
+//        assertTrue(emptyList.clear());
+//        assertTrue(listWithOneElement.clear());
+//    }
+//
+//    @Test
+//    void testToString(){
+//        String string = emptyList.toString();
+//        assertNull(string);
+//        string = listWithOneElement.toString();
+//        assertTrue(string.contains("Single"));
+//        string = listWithElements.toString();
+//        assertTrue(string.contains("Hi"));
+//        assertTrue(string.contains("Howdy"));
+//        assertTrue(string.contains("Hello"));
+//        assertTrue(string.contains("Morning"));
+//        listWithOneElement.remove("Single");
+//        string = listWithOneElement.toString();
+//        assertNull(string);
+//        listWithOneElement.add("Single");
+//        string = listWithOneElement.toString();
+//        assertNotNull(string);
+//    }
 }
