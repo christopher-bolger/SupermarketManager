ee887fa34433532410487c30dd2a51418e36658d
Christopher <chris09332@gmail.com>
2025-10-10 14:25:02 +0100

    Had to do a bit of testing here to try find the exact cause of my removal bug

--- src/main/java/supermarketmanager/controller/Main.java
+++ src/main/java/supermarketmanager/controller/Main.java
@@ -8,15 +8,11 @@ public class Main {
         test.add("A");
         test.add("B");
         test.add("C");
-        LinkedList<String> test2 = new LinkedList<>();
-        test2.add("D");
-        test2.add("E");
-        test2.add("F");
         for(String s : test) {
             System.out.println(s);
-            System.out.println("\n");
         }
-        test.addAll(1 ,test2);
+        boolean removed = test.remove("B");
+        System.out.println(removed);
         for(String s : test) {
             System.out.println(s);
         }
