b39a6479946ce4c4175328b7b3a7048e17a88329
Christopher <chris09332@gmail.com>
2025-10-14 11:56:25 +0100

    Yay more testing

---
+++ test/main/java/supermarketmanager/model/supermarket/MarketStructureTest.java
@@ -0,0 +1,75 @@
+package main.java.supermarketmanager.model.supermarket;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarketStructureTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getList() {
+    }
+
+    @Test
+    void setList() {
+    }
+
+    @Test
+    void setDimensions() {
+    }
+
+    @Test
+    void getDimensions() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void addAll() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+
+    @Test
+    void objectDetails() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
