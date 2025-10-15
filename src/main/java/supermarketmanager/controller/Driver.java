86d55db6bdd4dbb6a3ea7e204284555cacec8b64
Christopher <chris09332@gmail.com>
2025-10-15 11:00:23 +0100

    Working on console menu

--- src/main/java/supermarketmanager/controller/Driver.java
+++ src/main/java/supermarketmanager/controller/Driver.java
@@ -1,7 +1,11 @@
 package main.java.supermarketmanager.controller;
 
+import main.java.supermarketmanager.model.linkedlist.LinkedList;
 import main.java.supermarketmanager.model.supermarket.Floor;
+import main.java.supermarketmanager.model.supermarket.MarketStructure;
 import main.java.supermarketmanager.utils.ScannerInput;
+
+import java.util.List;
 
 public class Driver {
     private SupermarketManager manager;
@@ -28,15 +32,20 @@ public class Driver {
     }
 
     public void menu() throws Exception {
-        switch(showMenu()){
-            case 0 -> System.exit(0);
-            case 1 -> save();
-            case 2 -> load();
-            case 3 -> addFloor();
-            case 4 -> addAisle();
-            case 5 -> addShelf();
-            case 6 -> addGoodItem();
+        int option = showMenu();
+        while(option != 0) {
+            switch (option) {
+                case 1 -> save();
+                case 2 -> load();
+                case 3 -> addFloor();
+                //            case 4 -> addAisle();
+                //            case 5 -> addShelf();
+                //            case 6 -> addGoodItem();
+                case 7 -> showFloorInformation(getIndex());
+            }
+            option = showMenu();
         }
+        System.exit(0);
     }
 
     public int showMenu(){
@@ -52,16 +61,64 @@ public class Driver {
                 5) Add a Shelf
                 6) Add a GoodItem
                 ------------------------------
+                7) Show all Floors
+                8) Show Floor information
+                ------------------------------
                 """);
     }
 
     public void addFloor(){
-        String name = ScannerInput.readNextLine("Enter the name of your Floor Area: ");
+        showFloors();
+
+        boolean allowed = false;
+        String name = "";
+        do {
+            name = ScannerInput.readNextLine("Enter the name of your Floor Area: ");
+            if(manager.checkFloorName(name))
+                allowed = true;
+            if(!allowed)
+                System.out.println("Invalid Floor Name! Please try again!");
+        }while(!allowed);
+
         int[] dimensions = new int[2];
         dimensions[0] = ScannerInput.readNextInt(0, "Enter the x dimension: ");
         dimensions[1] = ScannerInput.readNextInt(0,"Enter the y dimension: ");
         int floorNumber = ScannerInput.readNextInt("Enter the floor level: ");
         manager.add(new Floor(name, dimensions, floorNumber));
+    }
+
+    public LinkedList<Floor> getFloors(){
+        return manager.getList();
+    }
+
+    public int getIndex(){
+        showFloors();
+        return ScannerInput.readNextInt(-1, "Which index would you like to select?");
+    }
+
+    public void showFloors(){
+        List<Floor> floors = getFloors();
+        if(floors.isEmpty())
+            return;
+        System.out.println("Floors: ");
+        int index = 0;
+        for(Floor floor : floors){
+            System.out.println("Index: " + index + ":" + " Level: " + floor.getFloor() + "\tName: " + floor.getName());
+            index++;
+        }
+    }
+
+    public void showFloorInformation(int index){
+        List<Floor> floors = getFloors();
+        if(floors.isEmpty()) {
+            System.out.println("No Floors, add one first!");
+            return;
+        }
+        Floor floor = floors.get(index);
+        if(floor == null)
+            System.out.println("Invalid index");
+        else
+            System.out.println(floors.get(index).toString());
     }
 
     public boolean loadData() {
@@ -71,9 +128,11 @@ public class Driver {
 
     public void load() throws Exception {
         manager.load();
+        menu();
     }
 
     public void save() throws Exception {
         manager.save();
+        menu();
     }
 }
