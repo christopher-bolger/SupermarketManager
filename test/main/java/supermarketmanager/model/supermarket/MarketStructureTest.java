c024d3b1b37183f3a9d08ce02f10ff709308907e
Christopher <chris09332@gmail.com>
2025-10-22 10:50:16 +0100

    Removed unused import

--- test/main/java/supermarketmanager/model/supermarket/MarketStructureTest.java
+++ test/main/java/supermarketmanager/model/supermarket/MarketStructureTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
