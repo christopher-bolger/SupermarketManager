1246b0d28db134c596478d29938902d0c7b698d5
chris <20111314@mail.wit.ie>
2025-11-02 19:44:45 +0000

    Getting somewhere with this, can open, save, save as & view tree Items

--- src/main/java/supermarketmanager/controller/MainController.java
+++ src/main/java/supermarketmanager/controller/MainController.java
@@ -5,8 +5,16 @@ import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
+import supermarketmanager.model.linkedlist.LinkedList;
+import supermarketmanager.model.supermarket.*;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainController {
+    SupermarketManager manager;
+    MarketStructure<?> selectedEntity;
     public VBox mainVBox;
     public MenuBar menuBar;
     public Menu fileButton;
@@ -22,26 +30,98 @@ public class MainController {
     public Button addShelf;
     public Button addGoodItem;
     public Button autoAddGoodItem;
-    public TextFlow errorOutput;
-    public TextFlow entityOutput;
-    public TreeView treeView;
+    public TextArea errorOutput;
+    public TitledPane superMarket;
+    public TextArea entityOutput;
+    public TreeView<MarketStructure<?>> treeView;
+    TreeItem<MarketStructure<?>> treeRoot;
 
     public void showMenu(ActionEvent actionEvent) {
     }
 
-    public void openExplorer(ActionEvent actionEvent) {
+    public void openExplorer(ActionEvent actionEvent) throws Exception {
+        if(manager == null){
+            manager = new SupermarketManager("Name", new int[] {100,100}, new File("untitled.xml"));
+        }
+        FileChooser file = new FileChooser();
+        file.setTitle("Open XML File");
+        file.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", "*.xml"));
+        File selected = file.showOpenDialog(menuBar.getScene().getWindow());
+        if (selected != null) {
+            manager.load(selected);
+            superMarket.setText(manager.getName());
+            updateTreeView();
+        }
     }
 
-    public void saveFile(ActionEvent actionEvent) {
+    public void updateTreeView(){
+        treeRoot = new TreeItem<>(manager);
+        treeView.setRoot(treeRoot);
+        treeView.setShowRoot(false);
+        getTreeChildren();
     }
 
-    public void saveAsFile(ActionEvent actionEvent) {
+    public void getTreeChildren(){
+        if(manager.isEmpty())
+            return;
+        for(Floor floor : manager.getList()) {
+            TreeItem<MarketStructure<?>> nextItem = new TreeItem<>(floor);
+            nextItem.setExpanded(true);
+            treeRoot.getChildren().add(nextItem);
+            if(!floor.isEmpty()) {
+                for(Aisle aisle : floor.getList()) {
+                    TreeItem<MarketStructure<?>> nextAisle = new TreeItem<>(aisle);
+                    nextAisle.setExpanded(true);
+                    nextItem.getChildren().add(nextAisle);
+                    if(!aisle.isEmpty()) {
+                        for(Shelf shelf : aisle.getList()) {
+                            TreeItem<MarketStructure<?>> nextShelf = new TreeItem<>(shelf);
+                            nextAisle.getChildren().add(nextShelf);
+                            if(!shelf.isEmpty()) {
+                                for(GoodItem goodItem : shelf.getList()) {
+                                    TreeItem<MarketStructure<?>> nextGoodItem = new TreeItem<>(goodItem);
+                                    nextShelf.getChildren().add(nextGoodItem);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void saveFile(ActionEvent actionEvent) throws Exception {
+        if(manager == null || manager.getFile() == null)
+            saveAsFile(actionEvent);
+        else
+            manager.save();
+    }
+
+    public void saveAsFile(ActionEvent actionEvent) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save As");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", "*.xml"));
+        fileChooser.setInitialFileName(manager.getName() + ".xml");
+        File file = fileChooser.showSaveDialog(menuBar.getScene().getWindow());
+        if (file != null) {
+            try{
+                manager.setFile(file);
+                manager.save();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public void exitApp(ActionEvent actionEvent) {
+        menuBar.getScene().getWindow().setOnCloseRequest(event -> {});
     }
 
     public void clearData(ActionEvent actionEvent) {
+        if(manager != null) {
+            manager.clear();
+            updateTreeView();
+        }
     }
 
     public void deleteSelectedEntity(ActionEvent actionEvent) {
@@ -66,5 +146,10 @@ public class MainController {
     }
 
     public void showSelectedEntity(MouseEvent mouseEvent) {
+        if(treeView.getSelectionModel().getSelectedItem().getValue() == null)
+            return;
+        selectedEntity = treeView.getSelectionModel().getSelectedItem().getValue();
+        entityOutput.setText(entityOutput.getText() + "\n" + selectedEntity);
+        mouseEvent.consume();
     }
 }
